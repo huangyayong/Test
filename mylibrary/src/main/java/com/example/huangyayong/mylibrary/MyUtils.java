d7bbf42c49b8b94fcfc1839bcaad59ac4d27f735
huangyayong <1140663504@qq.com>
2019-04-23 17:11:42 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/huangyayong/mylibrary/MyUtils.java
@@ -0,0 +1,10 @@
+package com.example.huangyayong.mylibrary;
+
+/**
+ * Created by huangyayong on 2019/4/23.
+ */
+public class MyUtils {
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
